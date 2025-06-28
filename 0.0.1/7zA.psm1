61621f620b1ecd0e1c237075f6fbf1176a4a2314
jesse kirk <jesse.kirk.1.ctr@us.af.mil>
2025-06-28 11:10:59 -0500

    Working rough draft of zipping up monthly updates

--- 0.0.1/7zA.psm1
+++ 0.0.1/7zA.psm1
@@ -1,12 +1,41 @@
-
+$i = Get-Module -ListAvailable -Refresh 7zA
+$json = Get-Content -Path (Join-Path -Path $i.ModuleBase -ChildPath settings.json) | ConvertFrom-Json
 function datetimeutc { return (Get-Date).ToUniversalTime().ToString('yyyyMMddTHHmmZ') }
 
-<#
+function sha256hashpackage
+{
+    $array = @()
+    (Get-ChildItem -Path $fullyqualifieddestinationpath -Recurse -File).FullName | ForEach-Object {
+        Write-Verbose -Message "Hashing $_ " -Verbose ; $i = Get-FileHash -Path $_ -Algorithm SHA256
+        $h = [pscustomobject]@{ name = $i.Path | Split-Path -Leaf ; hash = $i.Hash.ToLower() ; algorithm = $i.algorithm }
+        $array += $h
+    }
 
-7za.exe path /Dependencies/Build/7z/x64/7za.exe
+    $logfilepath = (Get-ChildItem -Path $fullyqualifieddestinationpath -Recurse -File -Filter '*.7z').FullName.Replace('.7z', '_Sha256_values.txt') ; $array | Format-List | Out-File -FilePath $logfilepath
+}
 
-Copy /Dependencies/Includes/*.* to the folder after 7.z package is built
+function buildreleasable
+{
+    $fullyqualifieddestinationpath = $fullyqualifieddestinationpath | Split-Path -Parent ; $fullyqualifieddestinationpath = $fullyqualifieddestinationpath += '\'
 
-& 7za.exe a -mx9 -mmt3 -md=2048m -m0=lzma2:fb64 -ms=16384m -bt "C:\Users\jesse.kirk\Desktop\Pack\SAMS-C2-v7.0.4_20250624T1316Z.7z" "C:\Users\jesse.kirk\Desktop\Pack\SAMS-C2-v7.0.4_20250624T1316Z\"
+    New-Item -Path $fullyqualifieddestinationpath -Name 'Release\bin' -ItemType Directory -Force -Verbose | Out-Null
+    Move-Item -Path $(Join-Path -Path $fullyqualifieddestinationpath -ChildPath '*.7z') -Destination $(Join-Path -Path $fullyqualifieddestinationpath -ChildPath 'Release') -Verbose
+    (Join-Path -Path $i.ModuleBase -ChildPath $json.includes -Resolve) | Copy-Item -Destination $(Join-Path -Path $fullyqualifieddestinationpath -ChildPath 'Release\bin') -Container -Verbose
+    sha256hashpackage
+}
 
-#>
+function build7zpackage
+{
+    [cmdletbinding()]
+    param([parameter(Mandatory)][string]$sourcepath)
+
+    if (-not(Test-Path -Path $sourcepath -ErrorAction SilentlyContinue)) { throw "The source path $sourcepath does not exist." }
+    if (-not($sourcepath.EndsWith('\'))) { $sourcepath += '\' }
+
+    $datetimeutc = datetimeutc
+    $destinationpath = ($sourcepath | Split-Path -Leaf) + '_' + $datetimeutc + '.7z'
+    $script:fullyqualifieddestinationpath = $sourcepath + $destinationpath
+
+    $files = Get-ChildItem -Path $sourcepath -Recurse ; $files | ForEach-Object { Write-Verbose -Message "Adding $_ to $destinationpath" -Verbose }
+    & (Join-Path -Path $i.ModuleBase -ChildPath $json.sevenZ -Resolve) $json.args $fullyqualifieddestinationpath $sourcepath ; buildreleasable
+}
