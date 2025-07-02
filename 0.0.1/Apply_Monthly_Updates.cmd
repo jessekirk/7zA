805dc56c3c70305d3c14df7592c1c362810b6f9d
jesse kirk <jesse.kirk.1.ctr@us.af.mil>
2025-07-02 07:41:24 -0500

    Updated to now use tar, xargs and ls to expand out the *.7z

--- 0.0.1/Apply_Monthly_Updates.cmd
+++ 0.0.1/Apply_Monthly_Updates.cmd
@@ -2,9 +2,8 @@
 
 pushd %~dp0
 powershell.exe -command New-Item -Path "C:\SIE\Authorized\Applications\Temp" -ItemType Directory -Force -Verbose
-powershell.exe -command Expand-Archive -Path "%~dp0SAMS-C2-v*.zip" -DestinationPath "C:\SIE\Authorized\Applications\Temp" -Force -Verbose
+bin\ls.exe *.7z | bin\xargs.exe tar --directory="C:\SIE\Authorized\Applications\Temp" -vxf
 cd "C:\SIE\Authorized\Applications\Temp" && cd SAMS*
 powershell.exe -command "& {Get-ChildItem -Path . -Recurse | Unblock-File -Verbose}" -executionpolicy bypass
-
 powershell.exe -file "Apply_Monthly_Updates.ps1" -executionpolicy bypass -windowstyle hidden
 popd && powershell.exe -command Remove-Item -Path "C:\SIE\Authorized\Applications\Temp\SAMS*" -Recurse -Force -Verbose
