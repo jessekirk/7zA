4a0c5c0bf6e9ae3735b0bf34326a2555fa5fba14
jesse kirk <jesse.kirk.1.ctr@us.af.mil>
2025-07-02 10:35:58 -0500

    Delete monthly update scripts. Those live in the gitlab/win11 repo

--- 0.0.1/Apply_Monthly_Updates.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-
-pushd %~dp0
-powershell.exe -command New-Item -Path "C:\SIE\Authorized\Applications\Temp" -ItemType Directory -Force -Verbose
-bin\ls.exe *.7z | bin\xargs.exe tar --directory="C:\SIE\Authorized\Applications\Temp" -vxf
-cd "C:\SIE\Authorized\Applications\Temp" && cd SAMS*
-powershell.exe -command "& {Get-ChildItem -Path . -Recurse | Unblock-File -Verbose}" -executionpolicy bypass
-powershell.exe -file "Apply_Monthly_Updates.ps1" -executionpolicy bypass -windowstyle hidden
-popd && powershell.exe -command Remove-Item -Path "C:\SIE\Authorized\Applications\Temp\SAMS*" -Recurse -Force -Verbose
