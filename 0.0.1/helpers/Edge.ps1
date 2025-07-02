eba8220bb4fcf3c31d03a2dce024fc3ed218d625
jesse kirk <jesse.kirk.1.ctr@us.af.mil>
2025-07-02 03:26:14 -0500

    Add uri back, change destination path to the user desktop

--- 0.0.1/helpers/Edge.ps1
+++ 0.0.1/helpers/Edge.ps1
@@ -1,12 +1,10 @@
-$settings = Get-Content -Path '..\settings.json' | ConvertFrom-Json
-$json = Invoke-RestMethod -Uri $settings.helpers.msEdge.uri
+$json = Invoke-RestMethod -Uri 'https://edgeupdates.microsoft.com/api/products?view=enterprise'
 
-$version = $json | Where-Object { $_.product -eq 'stable' } | Select-Object -ExpandProperty releases | Select-Object | Where-Object { $_.platform -eq 'windows' -and $_.architecture -eq 'x64' } | Sort-Object -Property productversion -Descending | Select-Object -First 1 | Select-Object -ExpandProperty productversion
-$urilocation = $json | Where-Object { $_.product -eq 'stable' } | Select-Object -ExpandProperty releases | Select-Object | Where-Object { $_.platform -eq 'windows' -and $_.architecture -eq 'x64' }`
+$version = $json | Where-Object { $_.product -eq 'stable' } | Select-Object -ExpandProperty releases | Select-Object | Where-Object { $_.platform -eq 'windows' -and $_.architecture -eq 'x64' } `
+| Sort-Object -Property productversion -Descending | Select-Object -First 1 | Select-Object -ExpandProperty productversion
+
+$urilocation = $json | Where-Object { $_.product -eq 'stable' } | Select-Object -ExpandProperty releases | Select-Object | Where-Object { $_.platform -eq 'windows' -and $_.architecture -eq 'x64' } `
 | Sort-Object -Property productversion -Descending | Select-Object -First 1 | Select-Object -ExpandProperty artifacts | Select-Object -ExpandProperty location
 
-$path = "$($(Get-Location).Path)\Microsoft Edge\v$version"
-$filename = $urilocation | Split-Path -Leaf
-
-New-Item -Path $path -ItemType Directory -Verbose | Out-Null
-Start-BitsTransfer -Source $urilocation -Destination $path\$filename -TransferType Download -Verbose
+$path = "$($HOME)\desktop\Microsoft Edge v$version" ; $filename = $urilocation | Split-Path -Leaf
+New-Item -Path $path -ItemType Directory -Verbose | Out-Null ; Start-BitsTransfer -Source $urilocation -Destination $path\$filename -TransferType Download -Verbose
